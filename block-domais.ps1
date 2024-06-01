8aae85aa98dd1141ac19b80451591aeef0bdd709
Mateus <69646100+mateus-sartorio@users.noreply.github.com>
2024-06-01 13:56:00 -0300

    Create block-domais.ps1

---
+++ block-domais.ps1
@@ -0,0 +1,5 @@
+Add-Content -Path "C:\Windows\System32\drivers\etc\hosts" -Value "0.0.0.0 krunker.io" -Encoding UTF8
+Add-Content -Path "C:\Windows\System32\drivers\etc\hosts" -Value "0.0.0.0 roblox.com" -Encoding UTF8
+Add-Content -Path "C:\Windows\System32\drivers\etc\hosts" -Value "0.0.0.0 www.roblox.com" -Encoding UTF8
+Add-Content -Path "C:\Windows\System32\drivers\etc\hosts" -Value "0.0.0.0 www.roblox.com/pt" -Encoding UTF8
+Add-Content -Path "C:\Windows\System32\drivers\etc\hosts" -Value "0.0.0.0 www.frivjogosonline.com.br" -Encoding UTF8
